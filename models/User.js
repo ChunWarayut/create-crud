cb270a0534e5f0bd1272154974e8748707b0629b
ChunWarayut <warayut2555@gmail.com>
2022-07-24 02:02:36 +0700

    ทดสอบ crud

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    
+    name: { type: String, default: '' },
+    nickname: { type: String, default: '' },
+    tel: { type: String, default: '' },
+    address: { type: String, default: '' },
+    age: { type: Number, default: 0 },
+
+}, { timestamps: true, versionKey: false })
+
+module.exports = mongoose.models.User || mongoose.model('User', UserSchema)
